7039b2441cdc062480650ab790253639fd193e3d
Damsa Paul <47825702+pauldamsa@users.noreply.github.com>
2020-03-18 15:34:28 +0200

    Add face.py in order to test on RPi

---
+++ face.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+import dlib
+import time
+
+cap = cv2.VideoCapture(0) # get frames from webcam
+detector = dlib.get_frontal_face_detector() # get frontal face detector from dlib
+
+while True:
+    start = time.time()
+    ret, frame = cap.read() # read from the video stream
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # transform every frame in gray scale image
+    
+    faces = detector(gray)
+
+    for face in faces:
+        x, y = face.left(), face.top()
+        w, h = face.right(), face.bottom()
+
+        cv2.rectangle(frame,(x, y), (w, h),(0,255,0),3)
+
+    cv2.imshow("cropped gray image", gray[y:h, x:w])
+    cv2.imshow("frame", frame)
+
+    if cv2.waitKey(1) == ord('s'):
+        break
+
+    end = time.time()
+    seconds = end - start
+    fps = 1.0 / seconds
+    print('fps: %.2f' % fps)
+
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
